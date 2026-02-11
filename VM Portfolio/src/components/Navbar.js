c0cfa0d312bb404b4c550e48e07ceb39f539bd4c
vay-tk <vaytk001@gmail.com>
2026-02-11 20:09:13 +0530

    Update Navbar.js

--- VM Portfolio/src/components/Navbar.js
+++ VM Portfolio/src/components/Navbar.js
@@ -19,14 +19,15 @@ function Navbar() {
         </label>
         <div className="wrapper">
           <ul>
-            <li><a href="#hero">Home</a></li>
-            <li><a href="#project">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">Contact</a></li>
           </ul>
         </div>
       </div>
     </nav>
   );
 }
+
 
 export default Navbar;
